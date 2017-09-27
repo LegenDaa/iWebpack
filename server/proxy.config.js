e068e68056142311214d881cfcbdba0b74a453b5
wb-daixiulei <daixiulei@163.com>
2017-09-27 14:59:30 +0800

    delete

--- server/proxy.config.js
+++
@@ -1,9 +0,0 @@
-//配置代理服务器的代理指向
-const proxy=[{
-    url:"/back_end/auth/",
-    target:"http://10.2.0.1:8351"
-},{
-    url:"/back_end/",
-    target:"http://10.2.0.1:8352"
-}]
-module.exports=proxy;
